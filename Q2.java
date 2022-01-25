a45630de0dcb3bd9d4c600ebf6b13c01ef70794e
2102580 <97870265+2102580@users.noreply.github.com>
2022-01-25 15:05:47 +0800

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,31 @@
+class Main{
+    public static void main(String[] args) {
+     System.out.println("Hello, I am Ho Zheng Wei ");
+
+     String module = "CSC1009";   
+        switch(module)   
+        { 
+            case "CSC1006":   
+                System.out.println("Mathematics 2"); 
+                break;
+            case "CSC1007":   
+                System.out.println("Opertaing Systems"); 
+                break;
+            case "CSC1008":   
+                System.out.println("Data Structures and Algorithms"); 
+                break;  
+            case "CSC1009":   
+                System.out.println("Object-Oriented Programming"); 
+                break;
+            case "CSC1010":   
+                System.out.println("Computer Networks"); 
+                break;   
+            
+        }   
+        for(int x=101;x>66;x=x-2){
+            System.out.print("Value of x : "+x );
+             System.out.print("\n");
+          }
+     
+   }
+ }
